c7b2059d707b77d12f040c8cdf76c42d0dc57370
Monarch Corps <monarchcorps01@gmail.com>
2025-03-24 01:57:20 +0100

    created a custom toast

---
+++ components/ToastItem.tsx
@@ -0,0 +1,116 @@
+import { useToast } from '@/context/ToastContext';
+import { useEffect, useRef } from 'react';
+import {
+    Animated,
+    StyleSheet,
+    Text,
+    View,
+    Pressable,
+} from 'react-native';
+
+export interface ToastData {
+    id: number;
+    title: string;
+    description?: string;
+    actionLabel?: string;
+    onAction?: () => void;
+    error?: boolean;
+}
+
+const ToastItem = ({
+    id,
+    title,
+    description,
+    actionLabel,
+    onAction,
+    index,
+    error = false
+}: ToastData & { index: number }) => {
+    const { removeToast } = useToast();
+
+    const translateY = useRef(new Animated.Value(-50)).current;
+    const opacity = useRef(new Animated.Value(0)).current;
+
+    const handleAction = () => {
+        if (onAction) onAction();
+        removeToast(id);
+    };
+
+    useEffect(() => {
+        Animated.parallel([
+            Animated.spring(translateY, {
+                toValue: index * 10,
+                useNativeDriver: true,
+            }),
+            Animated.timing(opacity, {
+                toValue: 1,
+                duration: 200,
+                useNativeDriver: true,
+            }),
+        ]).start();
+    }, [index]);
+
+    return (
+        <Animated.View
+            style={[
+                styles.toast,
+                {
+                    transform: [{ translateY }],
+                    opacity,
+                    backgroundColor: !error ? '#121212' : "#ff2828"
+                },
+            ]}
+        >
+            <View style={styles.content}>
+                <Text style={styles.title}>{title}</Text>
+                {description && (
+                    <Text style={styles.description}>{description}</Text>
+                )}
+            </View>
+
+            {actionLabel && (
+                <Pressable style={styles.actionButton} onPress={handleAction} className=''>
+                    <Text style={styles.actionText}>{actionLabel}</Text>
+                </Pressable>
+            )}
+        </Animated.View>
+    );
+};
+
+export default ToastItem;
+
+const styles = StyleSheet.create({
+    toast: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        borderRadius: 8,
+        padding: 12,
+        elevation: 4,
+        shadowColor: '#000',
+        shadowOpacity: 0.1,
+        shadowRadius: 10,
+    },
+    content: {
+        flex: 1,
+    },
+    title: {
+        color: '#fff',
+        fontWeight: '600',
+        fontSize: 16,
+    },
+    description: {
+        color: '#aaa',
+        fontSize: 14,
+    },
+    actionButton: {
+        marginLeft: 12,
+        backgroundColor: '#fff',
+        paddingHorizontal: 10,
+        paddingVertical: 6,
+        borderRadius: 6,
+    },
+    actionText: {
+        color: '#121212',
+        fontWeight: '600',
+    },
+});
---
+++ context/ToastContext.tsx
@@ -0,0 +1,68 @@
+import ToastItem, { ToastData } from '@/components/ToastItem';
+import {
+    createContext,
+    useContext,
+    useState,
+    ReactNode
+} from 'react';
+import { View, StyleSheet } from 'react-native';
+
+interface ToastContextType {
+    showToast: (toast: Omit<ToastData, 'id'>) => void;
+    removeToast: (id: number) => void,
+}
+
+const ToastContext = createContext<ToastContextType | undefined>(undefined);
+
+let toastId = 0;
+
+export const ToastProvider = ({ children }: { children: ReactNode }) => {
+    const [toasts, setToasts] = useState<ToastData[]>([]);
+
+    const showToast = (toast: Omit<ToastData, 'id'>) => {
+        const id = toastId++;
+        const newToast = { id, ...toast };
+
+        setToasts(prev => [newToast, ...prev]);
+
+        // Auto dismiss after 4s
+        setTimeout(() => {
+            setToasts(prev => prev.filter(t => t.id !== id));
+        }, 4000);
+    };
+
+    const removeToast = (id: number) => {
+        setToasts((prev) => prev.filter((t) => t.id !== id));
+    };
+
+    return (
+        <ToastContext.Provider value={{ showToast, removeToast }}>
+            {children}
+
+            {/* Toast Stack */}
+            <View style={styles.toastContainer}>
+                {toasts.map((toast, index) => (
+                    <ToastItem key={toast.id} {...toast} index={index} />
+                ))}
+            </View>
+        </ToastContext.Provider>
+    );
+};
+
+export const useToast = () => {
+    const context = useContext(ToastContext);
+    if (!context) {
+        throw new Error('useToast must be used inside ToastProvider');
+    }
+    return context;
+};
+
+const styles = StyleSheet.create({
+    toastContainer: {
+        position: 'absolute',
+        top: 50,
+        width: '100%',
+        paddingHorizontal: 16,
+        zIndex: 10000,
+    },
+});
