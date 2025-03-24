eea2b59546ba939c354c3908e7d7c047494b9aea
Monarch Corps <monarchcorps01@gmail.com>
2025-03-24 02:04:41 +0100

    created a config file

---
+++ configs/index.tsx
@@ -0,0 +1,10 @@
+const CONFIGS = {
+    ENVIRONMENT: process.env.EXPO_PUBLIC_ENVIRONMENT_MODE,
+    URL: {
+        // API_BASE_URL: process.env.EXPO_PUBLIC_API_BASE_URL,
+        API_BASE_URL: "https://74ff-102-89-44-105.ngrok-free.app",
+        API_VERSION: process.env.EXPO_PUBLIC_API_VERSION,
+    }
+}
+
+export { CONFIGS }
