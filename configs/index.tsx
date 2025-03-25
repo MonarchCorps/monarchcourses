5488e8b32b3d9808223dd4e2790357caa3b1127e
Monarch Corps <monarchcorps01@gmail.com>
2025-03-25 01:16:11 +0100

    used base url from env file

--- configs/index.tsx
+++ configs/index.tsx
@@ -1,8 +1,7 @@
 const CONFIGS = {
     ENVIRONMENT: process.env.EXPO_PUBLIC_ENVIRONMENT_MODE,
     URL: {
-        // API_BASE_URL: process.env.EXPO_PUBLIC_API_BASE_URL,
-        API_BASE_URL: "https://74ff-102-89-44-105.ngrok-free.app",
+        API_BASE_URL: process.env.EXPO_PUBLIC_API_BASE_URL,
         API_VERSION: process.env.EXPO_PUBLIC_API_VERSION,
     }
 }
