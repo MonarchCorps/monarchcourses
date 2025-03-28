bf0b6a3b3fcf1193bc02a706483c21f3290fdfda
Monarch Corps <monarchcorps01@gmail.com>
2025-03-28 03:18:53 +0100

    redirected to auth screen only if token is expired and there is still a refresh token in storage

--- app/index.tsx
+++ app/index.tsx
@@ -4,15 +4,21 @@ import TabsComponent from "./(tabs)";
 import OnboardComponent from "./(routes)/onboarding";
 import { useTokenExpired } from "@/hooks/useTokenExpired";
 import { Redirect } from "expo-router";
+import { getRefreshToken } from "@/helper/token";
 
 export default function Index() {
 
 	const { loading, auth } = useAuth()
 	const { isTokenExpired } = useTokenExpired()
+	let refreshToken: string | null = null;
+
+	(async (): Promise<string | null> => {
+		return refreshToken = await getRefreshToken();
+	})();
 	{/* <Redirect href={!auth?.user.id ? "/(routes)/onboarding" : "/(tabs)"} /> */ }
 
 	return (
-		isTokenExpired ?
+		!!refreshToken && isTokenExpired ?
 			<Redirect href="/sign-in" />
 			: loading
 				? <View className="size-full bg-black"></View>
